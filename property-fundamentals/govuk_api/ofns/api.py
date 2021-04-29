3168b2c72f3c7d7e0b58141f3d124f7a2b7c6310
Edward Gorman <ejgorman@gmail.com>
2021-04-29 17:32:52 +0100

    Update OFNS api to select by id

--- property-fundamentals/govuk_api/ofns/api.py
+++ property-fundamentals/govuk_api/ofns/api.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import urllib
 
@@ -13,6 +14,49 @@ class API:
     def __init__(self):
         self.url = 'https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/WGS84_UK_Wards_December_2017_Boundaries/MapServer/0'
         self.output = 'pjson'
+        self.district_ward_dict = {}
+
+        # Load admin_areas.csv file
+        with open('../data/external/admin_areas.csv') as csv_file:
+            csv_results = list(csv.reader(csv_file, delimiter=','))[1:]
+
+            # For each row in the csv file
+            for county_code, county, district_code, district, ward_code, ward, _, _ in csv_results:
+                if district not in self.district_ward_dict.keys():
+                    self.district_ward_dict[district] = ([], [])
+                self.district_ward_dict[district][0].append(ward)
+                self.district_ward_dict[district][1].append(ward_code)
+
+
+    def get_districts(self):
+        '''
+        Returns the districts that are present in admin_areas.csv.
+
+                Parameters:
+                    None
+                
+                Returns:
+                    districts (list): List of districts in alphabetical order.
+        '''
+        return sorted(list(self.district_ward_dict.keys()))
+    
+
+    def get_ward(self, district):
+        '''
+        Returns the wards that are present within a given district.
+
+                Parameters:
+                    None
+                
+                Returns:
+                    wards (list): List of wards in alphabetical order.
+        '''
+        if district == None:
+            raise Exception("Error: Need to specify a district.")
+        elif district not in self.district_ward_dict.keys():
+            raise Exception("Error: Could not find district '" + district + "' in the csv.")
+        
+        return sorted(self.district_ward_dict[district][0])
     
 
     def request(self, endpoint, parameters={}) -> dict:
@@ -33,26 +77,38 @@ class API:
         return result
 
 
-    def query(self, location) -> dict:
+    def get_ward_polygon(self, district, ward) -> dict:
         '''
-        Returns the API request for the query endpoint.
+        Returns the API request for retrieving ward polygons.
 
         https://geoportal.statistics.gov.uk/datasets/7193daa99995445aa84a0b23352e56a1_0/geoservice.
 
                 Parameters:
-                    where (str): The location to search (required).
+                    district (str): The district to find (required).
+                    ward (str): The ward to find (required).
                 
                 Returns:
-                    result (json): JSON formatted response.
+                    result (list): List of point for the ward polygon.
 
         '''
-        if location == None:
-            raise Exception("Error: Need to specify a location for 'query' endpoint.")
+        if district == None:
+            raise Exception("Error: Need to specify a district.")
+        elif district not in self.district_ward_dict.keys():
+            raise Exception("Error: Could not find district '" + district + "' in the csv.")
+
+        if ward == None:
+            raise Exception("Error: Need to specify a ward.")
+        elif ward not in self.district_ward_dict[district][0]:
+            raise Exception("Error: Could not find district '" + district + "' in the csv.")
+
+        ward_id = self.district_ward_dict[district][1][self.district_ward_dict[district][0].index(ward)]
 
         parameters = {
-            "where": "wd17nm='" + location + "'",
+            "where": "wd17cd='" + ward_id + "'",
             "geometryType": "esriGeometryPolygon",
             "f": self.output
         }
 
-        return self.request('query', parameters)
+        response = self.request('query', parameters)
+        polygon = response['features'][0]['geometry']['rings'][0]
+        return polygon
