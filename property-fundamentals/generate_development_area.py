377cd6c0564c22a5380aa09c29f8c3a1480a6e71
Edward Gorman <ejgorman@gmail.com>
2021-05-09 12:24:06 +0100

    Example python script for accessing sub-packages

---
+++ property-fundamentals/generate_development_area.py
@@ -0,0 +1,24 @@
+import simplekml
+from govuk_api.ofns.api import API
+
+kml = simplekml.Kml()
+pol1 = kml.newpolygon()
+pol2 = kml.newpolygon()
+pol3 = kml.newpolygon()
+pol4 = kml.newpolygon()
+pol5 = kml.newpolygon()
+pol6 = kml.newpolygon()
+pol7 = kml.newpolygon()
+pol8 = kml.newpolygon()
+pol9 = kml.newpolygon()
+pol10 = kml.newpolygon()
+pol11 = kml.newpolygon()
+pol12 = kml.newpolygon()
+pol13 = kml.newpolygon()
+pol14 = kml.newpolygon()
+pol15 = kml.newpolygon()
+pol16 = kml.newpolygon()
+pol17 = kml.newpolygon()
+pol18 = kml.newpolygon()
+
+print("made it")
