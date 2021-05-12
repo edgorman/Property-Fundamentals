358ef8b10a1b703b2d99222f2000672abb4eafe1
Will Gorman <wdgorman@gmail.com>
2021-05-12 21:16:36 +0100

    Added user input. try with Gloucester

--- property-fundamentals/generate_development_area.py
+++ property-fundamentals/generate_development_area.py
@@ -1,15 +1,19 @@
 from govuk_api.ofns.api import API
 ofns_api = API()
-#ofns_api.get_districts()
 
 coordinates = []
 wards = []
 
-wards.append(ofns_api.get_ward('Gloucester'))
+#Show the areas available
+print(ofns_api.get_districts())
 
+#Ask the user for an area
+area = input("Please type an area and press enter:")
+
+#Get the wards within an area
+wards.append(ofns_api.get_ward(area))
+
+#Get the coordinates of the wards
 for j in range (0,len(wards[0])):
-    coordinates.append(ofns_api.get_ward_polygon('Gloucester', wards[0][j]))
-    
-# with open('test.txt','w')as f:
-    # for element in coordinates:
-        # f.write("%s\n" % element)
+    coordinates.append(ofns_api.get_ward_polygon(area, wards[0][j]))
+   
