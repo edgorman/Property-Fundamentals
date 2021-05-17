99800e729a6acaca1c1ebab07215c74464c15eb2
Will Gorman <wdgorman@gmail.com>
2021-05-17 10:48:34 +0100

    its getting good!

--- property-fundamentals/generate_kml.py
+++ property-fundamentals/generate_kml.py
@@ -3,6 +3,7 @@ from generate_development_area import wards
 from generate_development_area import coordinates
 from generate_development_area import area
 from generate_property_price import price_median
+from generate_property_price import house_types
 
 #Define variables / lists
 kml = simplekml.Kml()
@@ -11,41 +12,46 @@ pol = []
 colour_scale = []
 price_scale = []
 
+#Generate / Draw KML polygons
+for h in range(0,len(coordinates)):
+    pol.insert(h,kml.newpolygon())
+    pol[h].name = wards[0][h]
+    pol[h].style.linestyle.width = "0"
+    pol[h].outerboundaryis.coords = coordinates[h]
+
 #Generate price scaling information
 max_price = max(price_median[0])
 min_price = min(price_median[0])
 print(max_price)
 print(min_price)
 delta = max_price - min_price
-step = delta / (len(colour)-1)
-
-#Generate KML polygon placeholders
-for h in range(0,len(wards)):
-    pol.insert(h,kml.newpolygon())
+step = delta / (len(colour)-1)    
 
 #Normalise the colours in the price range
 for i in range(0,len(colour)):
     colour_scale.insert(i,int(min_price + (i*step)))
 print(colour_scale)
+print(len(colour_scale))
+print(len(wards[0]))
+print(len(coordinates))
 
 #Assign a colour to the normalised house prices
-for j in range(0,len(wards)):
+for j in range(0,len(coordinates)):
+    #price_scale.append([])
     for k in range(0,len(colour_scale)):
         if (colour_scale[k] >= price_median[0][j]):
             price_scale.insert(j,colour[k])
             break
 print(price_scale)
 
-#Make the polygons
-for m in range(0,len(wards)):
-    pol[m].name = wards[0][m]
+#Add price and colour to the polygons
+for m in range(0,len(coordinates)):
     pol[m].description = "£" + str(price_median[0][m])
     pol[m].style.polystyle.color = price_scale[m]
-    pol[m].style.linestyle.width = "0"
-    pol[m].outerboundaryis.coords = coordinates[m] 
+
 
 #Save the polygons to a KML file
-kml.save(area + ".kml")
+kml.save(area + "_median_" + house_types[0][0] + ".kml")
 
 
 
--- property-fundamentals/generate_property_price.py
+++ property-fundamentals/generate_property_price.py
@@ -4,31 +4,13 @@ from generate_development_area import area
 median_price = MedianPrice()
 
 house_types = []
-
 price_median = []
-# median_price_all = []
-# median_price_detached = []
-# median_price_semi_detached = []
-# median_price_terraced = []
-# median_price_flats = []
-
 price_median_raw = []
-# median_price_all_raw = []
-# median_price_detached_raw = []
-# median_price_semi_detached_raw = []
-# median_price_terraced_raw = []
-# median_price_flats_raw = []
 
 house_types.append(median_price.get_house_types())
-print(house_types)
-print(house_types[0][0])
-print(house_types[0][1])
-print(house_types[0][2])
-print(house_types[0][3])
-print(house_types[0][4])
 
-# Median price all
-for h in range (0,4):
+# Median price
+for h in range (0,len(house_types[0])):
     price_median_raw.append([])
     price_median.append([])
     for i in range (0,len(wards[0])):
@@ -39,46 +21,6 @@ for h in range (0,4):
             price_median[h].insert(i,(int(float(price_median_raw[h][i]))))
 print(price_median)
 print(price_median_raw)
-
-# # Median price detached
-# for i in range (0,len(wards[0])):
-    # median_price_detached_raw.append(median_price.get_ward_data(area, wards[0][i], house_types[0][1]))
-    # if (median_price_detached_raw[i] == None):
-        # median_price_detached.append(0)
-    # else:
-        # median_price_detached.append(int(float(median_price_detached_raw[i])))
-# print(median_price_detached)
-# print(median_price_detached_raw)
-
-# # Median price semi-detached
-# for i in range (0,len(wards[0])):
-    # median_price_semi_detached_raw.append(median_price.get_ward_data(area, wards[0][i], house_types[0][2]))
-    # if (median_price_semi_detached_raw[i] == None):
-        # median_price_semi_detached.append(0)
-    # else:
-        # median_price_semi_detached.append(int(float(median_price_semi_detached_raw[i])))
-# print(median_price_semi_detached)
-# print(median_price_semi_detached_raw)
-
-# # Median price terraced
-# for i in range (0,len(wards[0])):
-    # median_price_terraced_raw.append(median_price.get_ward_data(area, wards[0][i], house_types[0][3]))
-    # if (median_price_terraced_raw[i] == None):
-        # median_price_terraced.append(0)
-    # else:
-        # median_price_terraced.append(int(float(median_price_terraced_raw[i])))
-# print(median_price_terraced)
-# print(median_price_terraced_raw)
-
-# # Median price flats
-# for i in range (0,len(wards[0])):
-    # median_price_flats_raw.append(median_price.get_ward_data(area, wards[0][i], house_types[0][4]))
-    # if (median_price_flats_raw[i] == None):
-        # median_price_flats.append(0)
-    # else:
-        # median_price_flats.append(int(float(median_price_flats_raw[i])))
-# print(median_price_flats)
-# print(median_price_flats_raw)
 
 #median_price_all.append(int(float(median_price.get_ward_data(area, wards[0][i], "all"))))
 
