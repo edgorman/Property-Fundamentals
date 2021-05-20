4ebcf64872b5efba955dbcdef16da8243c5add2f
Will Gorman <wdgorman@gmail.com>
2021-05-20 17:29:37 +0100

    corrected missing data algorithm

--- property-fundamentals/generate_missing_data.py
+++ property-fundamentals/generate_missing_data.py
@@ -5,8 +5,8 @@ from generate_property_price import house_types
 #Define variables / lists
 wards_neighbour = [] # [[0] * len(coordinates)] * len(coordinates)
 wards_neighbour_count = [0] * len(coordinates)
-#print(wards_neighbour)
-#print(wards_neighbour_count)
+print(wards_neighbour)
+print(wards_neighbour_count)
     
 #Find which wards are neighbours
 for b in range (0,len(coordinates)):
@@ -24,25 +24,29 @@ for b in range (0,len(coordinates)):
             else:
                 continue
             break
-#print(wards_neighbour)
-#print(wards_neighbour_count)
+print(wards_neighbour)
+print(wards_neighbour_count)
 
-
+print(price_mean)
 #For wards with missing data, take the average of the neighbouring wards which have data
-for n in range (0,len(house_types[0])):      
+for n in range (0,len(house_types[0])):
     for j in range(0,len(coordinates)):
         if price_mean[n][j] ==0:
             if wards_neighbour_count[j] !=0:
                 running_total=0
+                running_ward_count = wards_neighbour_count[j]
                 for k in range(0,len(coordinates)):
                     if wards_neighbour[j][k] == "Neighbour":
                         running_total += price_mean[n][k]
                         if price_mean[n][k] == 0:
-                            wards_neighbour_count[j] -= 1
-                price_mean[n][j] = int(float((running_total)/wards_neighbour_count[j]))
+                            running_ward_count -= 1
+                if running_ward_count < 1:
+                    price_mean[n][j] = int(float((sum(price_mean[n]))/(len(coordinates)-(price_mean[n].count(0)))))
+                else:
+                    price_mean[n][j] = int(float((running_total)/running_ward_count))
             else:
-                price_mean[n][j] = int(float((sum(price_mean[n]))/len(coordinates)))
+                price_mean[n][j] = int(float((sum(price_mean[n]))/(len(coordinates)-(price_mean[n].count(0)))))
 
 
-
+print(price_mean)
 
