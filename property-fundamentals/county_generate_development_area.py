88e6f5f54da31406dfb3c5dcb3d9da4614fa6e44
Will Gorman <wdgorman@gmail.com>
2021-05-31 22:01:47 +0100

    new colour scheme

--- property-fundamentals/county_generate_development_area.py
+++ property-fundamentals/county_generate_development_area.py
@@ -1,9 +1,9 @@
 from govuk_api.ofns.api import API
 ofns_api = API()
 
-coordinates = []
 districts = []
 wards = []
+max_ward_length = 0
 
 #Show the counties available
 print(ofns_api.get_counties())
@@ -14,22 +14,27 @@ county = input("Please type a county and press enter:")
 #Get the districts within a county
 districts.append(ofns_api.get_districts_from_county(county))
 
-print(districts)
-
 #Get the wards within a district
 for j in range (0,len(districts[0])):
     wards.append(ofns_api.get_wards_from_district(districts[0][j]))
 
-print(wards)
-print(len(wards))
-print(len(wards[0]))
+for m in range (0, len(wards)):
+    if (len(wards[m]) > max_ward_length):
+        max_ward_length = len(wards[m])
+print(max_ward_length)
+        
+coordinates = [[] for i in range (len(wards))]
 
+for n in range (0, len(wards)):
+    coordinates[n] = [[] for i in range (max_ward_length)]
+    
 #Get the coordinates of the wards
-for j in range (0,len(districts[0])):
-    coordinates.append([])
-    for k in range (0,len(wards[j])):
-        print(districts[0][j])
-        print(wards[j][k])
-        coordinates.append(ofns_api.get_ward_polygon(districts[0][j], wards[j][k]))
-   
+print(len(wards))
 print(coordinates)
+print(coordinates[0])
+print(coordinates[0][0])
+for j in range (0,len(wards)):
+    for m in range (0, max_ward_length):
+        for k in range (0,len(wards[j])):
+            coordinates[j][m].insert(k, ofns_api.get_ward_polygon(districts[0][j], wards[j][k]))
+# print(coordinates)
---
+++ property-fundamentals/county_generate_schools.py
@@ -0,0 +1,36 @@
+from govuk_ws.ofsted.schoolratings import SchoolRatings
+from doogal.api import API
+#from generate_development_area import wards
+from county_generate_development_area import county
+import simplekml
+import zipfile
+school_ratings = SchoolRatings()
+doogal_api = API()
+kml = simplekml.Kml()
+
+#school_ratings.get_districts()
+#school_ratings.get_schools_from_district(area)
+#school_ratings.get_schools_with_coordinates_from_district(doogal_api, 'Gloucester')
+
+point = []
+icon_style = ['images/icon-1.png', 'images/icon-2.png', 'images/icon-3.png', 'images/icon-4.png']
+ofsted_rating = ['Outstanding', 'Good', 'Requires improvement', 'Poor']
+
+school_data = school_ratings.get_schools_with_coordinates_from_district(doogal_api, county)
+
+for name, postcode, rating, ward, school_coordinates in school_data:
+    point = kml.newpoint()
+    point.name = name
+    point.description = ofsted_rating[int(rating)-1]
+    point.coords = [school_coordinates]
+    point.style.iconstyle.icon.href = icon_style[int(rating)-1] 
+kml.save(area + "_school_rating" + ".kml")
+
+zf = zipfile.ZipFile(area + "_school_rating" + ".kmz", "w")
+zf.write("images/icon-1.png")
+zf.write("images/icon-2.png")
+zf.write("images/icon-3.png")
+zf.write("images/icon-4.png")
+zf.write(area + "_school_rating" + ".kml")
+zf.close()
+
