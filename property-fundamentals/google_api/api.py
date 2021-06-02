9c97400b3db021bc6c7e2efaf7f3f3ab57e94d24
Edward Gorman <ejgorman@gmail.com>
2021-06-02 22:19:43 +0100

    Add nearby search endpoint to google api, updated notebook

--- property-fundamentals/google_api/api.py
+++ property-fundamentals/google_api/api.py
@@ -15,6 +15,18 @@ class API:
     def __init__(self, key=None, key_path=None):
         self.url = 'https://maps.googleapis.com/maps/api'
         self.output = 'json'
+        self.valid_types = ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'atm', 'bakery', 'bank', 
+        'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 'campground', 'car_dealer', 
+        'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetery', 'church', 'city_hall', 'clothing_store', 'convenience_store', 
+        'courthouse', 'dentist', 'department_store', 'doctor', 'drugstore', 'electrician', 'electronics_store', 'embassy', 
+        'fire_station', 'florist', 'funeral_home', 'furniture_store', 'gas_station', 'gym', 'hair_care', 'hardware_store', 
+        'hindu_temple', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 
+        'light_rail_station', 'liquor_store', 'local_government_office', 'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 
+        'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 
+        'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 'post_office', 'primary_school', 'real_estate_agency', 
+        'restaurant', 'roofing_contractor', 'rv_park', 'school', 'secondary_school', 'shoe_store', 'shopping_mall', 'spa', 
+        'stadium', 'storage', 'store', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 'tourist_attraction', 
+        'train_station', 'transit_station', 'travel_agency', 'university', 'veterinary_care', 'zoo']
 
         if key != None:
             self.key = key
@@ -23,7 +35,22 @@ class API:
                 self.key = key_file.readline()
         else:
             raise Exception("Error: Need to specify a key or path to file containing key.")
-    
+
+
+    def get_place_types(self):
+        '''
+        Returns the list of valid types used in the places search api.
+
+        https://developers.google.com/maps/documentation/places/web-service/supported_types
+
+                Parameters:
+                    None
+                
+                Returns:
+                    types (list): List of place types.
+        '''
+        return self.valid_types
+
 
     def request(self, endpoint, parameters={}) -> dict:
         '''
@@ -50,7 +77,7 @@ class API:
         '''
         Returns the API request for the 'find place from text' endpoint.
 
-        https://developers.google.com/maps/documentation/places/web-service/search.
+        https://developers.google.com/maps/documentation/places/web-service/search#FindPlaceRequests
 
                 Parameters:
                     input_ (str): The location to search (required).
@@ -63,9 +90,6 @@ class API:
                     result (json): JSON formatted response.
 
         '''
-        if input_ == None:
-            raise Exception("Error: Need to specify an input for 'find_place_from_text' endpoint.")
-
         parameters = {
             "input": input_,
             "inputtype": input_type,
@@ -78,3 +102,33 @@ class API:
                 parameters["location_area"] = location_area
         
         return self.request('place/findplacefromtext', parameters)
+
+
+    def nearby_search(self, lat, lon, radius, type_=None) -> dict:
+        '''
+        Returns the API request for the 'nearby search' endpoint.
+
+        https://developers.google.com/maps/documentation/places/web-service/search#PlaceSearchRequests
+
+                Parameters:
+                    lat (float): The latitude of the circle centre point.
+                    lon (float): The longitude of the circle centre point.
+                    radius (float): The radius of the circle to search from (in metres).
+                    type_ (str): Restricts the google api to a type of location.
+                
+                Returns:
+                    result (json): JSON formatted response.
+
+        '''
+        if type_ != None and type_ not in self.valid_types:
+            raise Exception("Error: The type '" + type_ + "' is not valid for the 'nearby search' endpoint.")
+        
+        parameters = {
+            "location": f"{lat},{lon}",
+            "radius": radius
+        }
+
+        if type_ != None:
+            parameters['type'] = type_
+        
+        return self.request('place/nearbysearch', parameters)
