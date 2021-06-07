24cf0fdbc2f1547de2d2c728ea592cbb01e8f826
Will Gorman <wdgorman@gmail.com>
2021-06-07 13:03:53 +0100

    trying doogal

--- property-fundamentals/generate_development_area.py
+++ property-fundamentals/generate_development_area.py
@@ -1,19 +1,19 @@
-from govuk_api.ofns.api import API
-ofns_api = API()
+from doogal.api import API
+doogal_api = API()
 
 coordinates = []
 wards = []
 
 #Show the districts available
-print(ofns_api.get_districts())
+print(doogal_api.get_districts())
 
 #Ask the user for a district
 area = input("Please type an area and press enter:")
 
 #Get the wards within a district
-wards.append(ofns_api.get_wards_from_district(area))
+wards.append(doogal_api.get_wards_from_district(area))
 
 #Get the coordinates of the wards
 for j in range (0,len(wards[0])):
-    coordinates.append(ofns_api.get_ward_polygon(area, wards[0][j]))
+    coordinates.append(doogal_api.get_ward_polygon(area, wards[0][j]))
    
