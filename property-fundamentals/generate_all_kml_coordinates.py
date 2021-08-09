f78a2a1c1c730e1ad2b3dd8eb1e9a86b5b22ee83
Will Gorman <wdgorman@gmail.com>
2021-08-09 15:38:38 +0100

    got code working to get coordinates in a txt file

--- property-fundamentals/generate_all_kml_coordinates.py
+++ property-fundamentals/generate_all_kml_coordinates.py
@@ -12,28 +12,28 @@ district = ofns_api.get_districts()
 print(district)
 
 #Ask the user for a district
-Number_string = input("Please type an district number:")
-Number = int(Number_string)
+k_string = input("Please type an district number:")
+k = int(k_string)
 
-wards.append(ofns_api.get_wards_from_district(district[Number]))
+wards.append(ofns_api.get_wards_from_district(district[k]))
 
 for j in range (0,len(wards[0])):
     try:
-        coordinates.append(ofns_api.get_ward_polygon(district[Number], wards[0][j]))
+        coordinates.append(ofns_api.get_ward_polygon(district[k], wards[0][j]))
     except ValueError as e:
         if str(e) == "Error: Need to specify a district.":
             print("Error: Need to specify a district.")
-        elif str(e) == "Error: Could not find district '" + district[Number] + "' in the csv.":
-            print("Error: Could not find district '" + district[Number] + "' in the csv.")
+        elif str(e) == "Error: Could not find district '" + district[k] + "' in the csv.":
+            print("Error: Could not find district '" + district[k] + "' in the csv.")
         elif str(e) == "Error: Need to specify a ward.":
             print("Error: Need to specify a ward.")
-        elif str(e) == "Error: Could not find ward '" + wards[0][j] + "' from district '" + district[Number] + "'.":
-            print("Error: Could not find ward '" + wards[0][j] + "' from district '" + district[Number] + "'.")
-        elif str(e) == "Error: The OFNS server has no coordinate data for ward '" + wards[0][j] + "' from district '" + district[Number] + "'.":
+        elif str(e) == "Error: Could not find ward '" + wards[0][j] + "' from district '" + district[k] + "'.":
+            print("Error: Could not find ward '" + wards[0][j] + "' from district '" + district[k] + "'.")
+        elif str(e) == "Error: The OFNS server has no coordinate data for ward '" + wards[0][j] + "' from district '" + district[k] + "'.":
             print("Using Doogal API")
-            coordinates.append(doogal_api.get_ward_polygon(district[Number], wards[0][j]))
+            coordinates.append(doogal_api.get_ward_polygon(district[k], wards[0][j]))
 
-with open("C:/Users/Anchal Goel/Desktop/Coordinates/" + district[Number] + "_coordinates.txt", "w") as f:
+with open("C:/Users/Anchal Goel/Desktop/Coordinates/" + district[k] + ".txt", "w") as f:
     for item in coordinates:
         f.write("%s\n" % coordinates)
 coordinates.clear()
