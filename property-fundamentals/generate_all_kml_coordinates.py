d21be32bb1bbe512801162e51ded2dcd52e801ca
Will Gorman <wdgorman@gmail.com>
2021-08-11 17:27:48 +0100

    try

--- property-fundamentals/generate_all_kml_coordinates.py
+++ property-fundamentals/generate_all_kml_coordinates.py
@@ -4,6 +4,11 @@ ofns_api = OFNS_API()
 doogal_api = DOOGAL_API()
 import geopy.distance
 import numpy as np
+from govuk_api.ofns.api import NoDistrictError
+from govuk_api.ofns.api import MissingDistrictError
+from govuk_api.ofns.api import NoWardError
+from govuk_api.ofns.api import MissingWardError
+from govuk_api.ofns.api import NoOFNSDataError
 
 coordinates = []
 wards = []
@@ -12,31 +17,33 @@ district = ofns_api.get_districts()
 print(district)
 
 #Ask the user for a district
-k_string = input("Please type an district number:")
-k = int(k_string)
+#k_string = input("Please type an district number:")
+#k = int(k_string)
 
-wards.append(ofns_api.get_wards_from_district(district[k]))
+#wards.append(ofns_api.get_wards_from_district(district[k]))
 
-for j in range (0,len(wards[0])):
-    try:
-        coordinates.append(ofns_api.get_ward_polygon(district[k], wards[0][j]))
-    except ValueError as e:
-        if str(e) == "Error: Need to specify a district.":
-            print("Error: Need to specify a district.")
-        elif str(e) == "Error: Could not find district '" + district[k] + "' in the csv.":
-            print("Error: Could not find district '" + district[k] + "' in the csv.")
-        elif str(e) == "Error: Need to specify a ward.":
-            print("Error: Need to specify a ward.")
-        elif str(e) == "Error: Could not find ward '" + wards[0][j] + "' from district '" + district[k] + "'.":
-            print("Error: Could not find ward '" + wards[0][j] + "' from district '" + district[k] + "'.")
-        elif str(e) == "Error: The OFNS server has no coordinate data for ward '" + wards[0][j] + "' from district '" + district[k] + "'.":
-            print("Using Doogal API")
-            coordinates.append(doogal_api.get_ward_polygon(district[k], wards[0][j]))
+for k in range (0,len(district)):
+    print(k)
+    wards.append(ofns_api.get_wards_from_district(district[k])) 
+    for j in range (0,len(wards[0])):
+        try:
+            coordinates.append(ofns_api.get_ward_polygon(district[k], wards[0][j]))
+        except NoDistrictError as e:
+                print("Error: Need to specify a district.")
+        except MissingDistrictError as e:
+                print("Error: Could not find district '" + district[k] + "' in the csv.")
+        except NoWardError as e:
+                print("Error: Need to specify a ward.")
+        except MissingWardError as e:
+                print("Error: Could not find ward '" + wards[0][j] + "' from district '" + district[k] + "'.")
+        except NoOFNSDataError as e:
+                print("Using Doogal API")
+                coordinates.append(doogal_api.get_ward_polygon(district[k], wards[0][j]))
 
-with open("C:/Users/Anchal Goel/Desktop/Coordinates/" + district[k] + ".txt", "w") as f:
-    for item in coordinates:
-        f.write("%s\n" % coordinates)
-coordinates.clear()
-
+    with open("C:/Users/Anchal Goel/Desktop/Coordinates/" + district[k] + ".txt", "w") as f:
+        for item in coordinates:
+            f.write("%s\n" % coordinates)
+    coordinates.clear()
+    wards.clear()
 
 
