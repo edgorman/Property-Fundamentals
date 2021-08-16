467f0b4e0c2d6aeb6d6189d84ad7176d5d138ea2
Will Gorman <wdgorman@gmail.com>
2021-08-16 18:54:05 +0100

    test

--- property-fundamentals/generate_google_earth_map.py
+++ property-fundamentals/generate_google_earth_map.py
@@ -3,12 +3,8 @@ from development_district import wards
 from development_district import coordinates
 from development_district import district
 
-
 #Define variables / lists
 kml = simplekml.Kml()
-#colour = ['4C14E7FF','4C14DEFF','4C14D5FF','4C14CCFF','4C14C3FF','4C14BAFF','4C14B1FF','4C14A8FF','4C14A0FF','4C1497FF','4C148EFF','4C1485FF','4C147CFF','4C1473FF','4C146AFF','4C1461FF','4C1458FF','4C1450FF','4C1447FF','4C143EFF','4C1435FF','4C142CFF','4C1423FF','4C141AFF','4C1411FF','4C1408FF','4C1400FF']
-colour = ['19F07814', '1EF07814', '23F07814', '28F07814', '2DF07814', '32F07814', '37F07814', '3CF07814', '41F07814', '46F07814', '4BF07814', '50F07814', '55F07814' ,'5AF07814', '5FF07814', '64F07814', '69F07814', '6EF07814', '73F07814', '78F07814', '7DF07814', '82F07814', '87F07814', '8CF07814', '91F07814', '96F07814', '9BF07814', 'A0F07814', 'A5F07814', 'AAF07814']
-
 pol = []
 
 #Generate / Draw polygons
@@ -17,7 +13,7 @@ for h in range(0,len(coordinates)):
     pol[h].name = wards[0][h]
     pol[h].style.linestyle.width = "0"
     pol[h].outerboundaryis.coords = coordinates[h]
-    pol[h].style.polystyle.color = '19F07814'
+    pol[h].style.polystyle.color = '64FFFFFF'#'0FF07814'
     
 kml.save(district + "_Google_Earth" + ".kml") 
 
