eba7e2e2b1250795e302988cf50da35e695d5c27
Will Gorman <wdgorman@gmail.com>
2021-09-09 12:28:33 +0100

    change font size for districts with many wards

--- property-fundamentals/generate_property_price.py
+++ property-fundamentals/generate_property_price.py
@@ -76,9 +76,10 @@ for n in range (0,len(house_types[0])):
         colouraxis.insert(j,price_plot[a])
     
     #plot the data
-    #plt.rcParams["figure.figsize"] = (4.5,5.5) # if there are many wards
-    plt.rcParams["figure.figsize"] = (4.5,5)
+    plt.rcParams["figure.figsize"] = (4.5,5.5) # if there are many wards
+    #plt.rcParams["figure.figsize"] = (4.5,5)
     plt.rcParams["figure.dpi"] = 200
+    plt.rc('ytick', labelsize=4)
     plt.locator_params(axis='x', nbins=4)
     plt.ticklabel_format(useOffset=False, style='plain')
     plt.barh(y_pos, xaxis, color= colouraxis, edgecolor='black')
