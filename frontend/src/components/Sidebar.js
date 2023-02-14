266a2541253d9824875aee2108250320cc61e618
Edward Gorman <ejgorman@gmail.com>
2023-02-14 19:15:39 +0000

    Add multiple pages to sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,6 +4,11 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Locations from "./Locations";
 import SidebarToggle from './SidebarToggle';
 
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+
 
 export default class Sidebar extends React.Component {
     constructor(props){
@@ -18,7 +23,38 @@ export default class Sidebar extends React.Component {
 
         return (
             <div className={classNames}>
-                <div className="content">
+                <Tab.Container defaultActiveKey="first">
+                    <Tab.Content className="content">
+                        <Tab.Pane eventKey="first">
+                            <Locations />
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                            <p>second</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                            <p>third</p>
+                        </Tab.Pane>
+                    </Tab.Content>
+                    <Nav className="buttons d-flex align-items-center">
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Locations</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            /
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Features</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            /
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">Explanation</Nav.Link>
+                        </Nav.Item>
+                        <SidebarToggle open={false} handleToggle={this.props.handleToggle} />
+                    </Nav>
+                </Tab.Container>
+                {/* <div className="content">
                     <Locations />
                 </div>
                 <div className="buttons d-flex align-items-center">
@@ -28,7 +64,7 @@ export default class Sidebar extends React.Component {
                         <Breadcrumb.Item>Explanation</Breadcrumb.Item>
                     </Breadcrumb>
                     <SidebarToggle open={false} handleToggle={this.props.handleToggle} />
-                </div>
+                </div> */}
             </div>
         )
     }
